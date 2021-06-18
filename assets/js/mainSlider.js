272e1664b330a184bd11d6afa8ed0d1cd3486f1d
Elsium <li.dsen@students.dvfu.ru>
2021-06-18 16:38:10 +1000

    adaptive for mobile

--- assets/js/mainSlider.js
+++ assets/js/mainSlider.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
-	$('.slider').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		dots: true,
-		appendDots: $('.main__popular__dots'),
-	});
+	let wid = $('.box').css('width');
+	wid = parseInt(wid);
+	if (wid >= 750) {
+		$('.slider').slick({
+			slidesToShow: 2,
+			slidesToScroll: 2,
+			arrows: false,
+			dots: true,
+			appendDots: $('.main__popular__dots'),
+		});
+	} else {
+		$('.slider').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			dots: true,
+			appendDots: $('.main__popular__dots'),
+		});
+	}
 });
 $('.main__popular__control_prev').on('click', function() {
 	$('.slider').slick('slickPrev');
