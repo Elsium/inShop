741c5d3fad54e61653dd90832597e3aa54923696
Elsium <li.dsen@students.dvfu.ru>
2021-06-19 09:33:01 +1000

    main n social adaptive full end

--- assets/js/mainSlider.js
+++ assets/js/mainSlider.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
 	wid = parseInt(wid);
 	if (wid >= 750) {
 		$('.slider').slick({
-			slidesToShow: 2,
-			slidesToScroll: 2,
+			slidesToShow: 4,
+			slidesToScroll: 4,
 			arrows: false,
 			dots: true,
 			appendDots: $('.main__popular__dots'),
 		});
 	} else {
 		$('.slider').slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
+			slidesToShow: 2,
+			slidesToScroll: 2,
 			arrows: false,
 			dots: true,
 			appendDots: $('.main__popular__dots'),
@@ -24,4 +24,32 @@ $('.main__popular__control_prev').on('click', function() {
 });
 $('.main__popular__control_next').on('click', function() {
 	$('.slider').slick('slickNext');
+});
+
+$(document).ready(function() {
+	let wid = $('.box').css('width');
+	wid = parseInt(wid);
+	if (wid >= 750) {
+		$('.slider_news').slick({
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			arrows: false,
+			dots: true,
+			appendDots: $('.main__news__dots'),
+		});
+	} else {
+		$('.slider_news').slick({
+			slidesToShow: 2,
+			slidesToScroll: 2,
+			arrows: false,
+			dots: true,
+			appendDots: $('.main__news__dots'),
+		});
+	}
+});
+$('.main__news__control_prev').on('click', function() {
+	$('.slider_news').slick('slickPrev');
+});
+$('.main__news__control_next').on('click', function() {
+	$('.slider_news').slick('slickNext');
 });
