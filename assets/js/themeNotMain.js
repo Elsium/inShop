6e92cbebc96583cb60c8e78c5bc9ad0457cb0231
Elsium <li.dsen@students.dvfu.ru>
2021-06-22 03:31:58 +1000

    cookie

--- assets/js/themeNotMain.js
+++ assets/js/themeNotMain.js
@@ -1,7 +1,22 @@
+function getCookie(name) {
+	let matches = document.cookie.match(new RegExp(
+		"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+	));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+var theme = getCookie("theme");
 var btn = document.getElementById("theme-button");
+var btn_mobile = document.getElementById("theme-button-mobile");
 var link = document.getElementById("theme-link");
 
+if (theme == "dark") {
+	link.setAttribute("href", "../assets/css/darkTheme.css");
+} else if (theme == "light") {
+	link.setAttribute("href", "../assets/css/lightTheme.css");
+}
+
 btn.addEventListener("click", function () { ChangeTheme(); });
+btn_mobile.addEventListener("click", function () { ChangeTheme(); });
 
 function ChangeTheme()
 {
@@ -14,15 +29,13 @@ function ChangeTheme()
 	if(currTheme == lightTheme)
 	{
 		currTheme = darkTheme;
-		theme = "dark";
+		document.cookie = "theme=dark; path=/";
 	}
 	else
 	{
 		currTheme = lightTheme;
-		theme = "light";
+		document.cookie = "theme=light; path=/";
 	}
 	
 	link.setAttribute("href", currTheme);
-	
-	Save(theme);
 }
